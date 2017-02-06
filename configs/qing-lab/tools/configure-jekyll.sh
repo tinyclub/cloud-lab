0342b82466a5b3afd414e5773d8e24bdcd6b119b
Wu Zhangjin <wuzhangjin@gmail.com>
2017-02-07 02:12:46 +0800

    tinylab.org: Rebase on tinylab/cloud-ubuntu-jekyll
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- configs/qing-lab/tools/configure-jekyll.sh
+++ configs/qing-lab/tools/configure-jekyll.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-JEKYLL_SUPERVISOR_CONF=/etc/supervisor/conf.d/jekyll.conf
-
 container_ip=$(ip addr show eth0 | head -3 | tail -1 | sed -e "s%.*inet \(.*\)/.*%\1%g")
-sed -i -e "s% -H HOST_IP% -H $container_ip%g" $JEKYLL_SUPERVISOR_CONF
-echo "LOG: Jekyll Local Web Site Address: http://$container_ip/"
 
 DESKTOP=/home/$UNIX_USER/Desktop/
 local_shortcut_count=`find $DESKTOP -name local.desktop | wc -l`
