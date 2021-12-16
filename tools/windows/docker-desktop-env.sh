e1864bb7ea56a49d7cd07b3ddfba86c8ee24c90f
Wu Zhangjin <falcon@tinylab.org>
2021-12-17 04:34:06 +0800

    tools: windows: easier the docker toolbox and desktop switch
    we can simply write "toolbox" or "desktop" to .docker_name to start the switch.
    
    Signed-off-by: Wu Zhangjin <falcon@tinylab.org>

--- tools/windows/docker-desktop-env.sh
+++ tools/windows/docker-desktop-env.sh
@@ -6,6 +6,8 @@
 which docker-machine >/dev/null 2>&1
 if [ $? -eq 0 ]; then
   DOCKER_TOOLBOX_PATH="$(dirname "$(which docker-machine)")"
+elif [ -n "$DOCKER_TOOLBOX_INSTALL_PATH" ]; then
+  DOCKER_TOOLBOX_PATH="$DOCKER_TOOLBOX_INSTALL_PATH"
 else
   DOCKER_TOOLBOX_PATH="/c/Program Files/Docker Toolbox"
 fi
@@ -17,6 +19,7 @@ if [ -f "$DOCKER_TOOLBOX_PATH"/docker-machine.exe ]; then
   unset DOCKER_CERT_PATH
   unset DOCKER_MACHINE_NAME
   unset COMPOSE_CONVERT_WINDOWS_PATHS
+  unset DOCKER_TOOLBOX_INSTALL_PATH
 
   export PATH="$(echo $PATH | sed -e "s%$DOCKER_TOOLBOX_PATH/:%%g;s%$DOCKER_TOOLBOX_PATH:%%g")"
 fi
@@ -24,12 +27,23 @@ fi
 which docker > /dev/null 2>&1
 if [ $? -eq 0 ]; then
   DOCKER_DESKTOP_PATH="$(dirname "$(which docker)")"
+else
+  DOCKER_DESKTOP_PATH="/c/Program Files/Docker/Docker/resources/bin"
+fi
+
+if [ -d "$DOCKER_DESKTOP_PATH" ]; then
   DOCKER_DESKTOP_TOP_DIR="$(cd "$DOCKER_DESKTOP_PATH"/../../ && pwd)"
   DOCKER_DESKTOP="$DOCKER_DESKTOP_TOP_DIR/Docker Desktop.exe"
 
   if [ -f "$DOCKER_DESKTOP" ]; then
     echo "Init Docker Desktop"
     "$DOCKER_DESKTOP" &
+
+    echo $PATH | grep -q "$DOCKER_DESKTOP_PATH"
+    if [ $? -ne 0 ]; then
+      echo "Add path of docker desktop to global PATH"
+      export PATH="$DOCKER_DESKTOP_PATH:$PATH"
+    fi
   else
     echo "No Docker Desktop found, you can try Docker Toolbox with tools/windows/docker-toolbox-env.sh"
   fi
--- tools/windows/docker-toolbox-env.sh
+++ tools/windows/docker-toolbox-env.sh
@@ -6,8 +6,20 @@
 which docker-machine >/dev/null 2>&1
 if [ $? -eq 0 ]; then
   DOCKER_TOOLBOX_PATH="$(dirname "$(which docker-machine)")"
+elif [ -n "$DOCKER_TOOLBOX_INSTALL_PATH" ]; then
+  DOCKER_TOOLBOX_PATH="$DOCKER_TOOLBOX_INSTALL_PATH"
 else
   DOCKER_TOOLBOX_PATH="/c/Program Files/Docker Toolbox"
+fi
+
+docker_path="$(which docker)" >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+  echo "$docker_path" | grep -q "Docker Toolbox"
+  if [ $? -ne 0 ]; then
+    echo "LOG: Remove path of docker desktop out of global PATH"
+    DOCKER_DESKTOP_PATH="$(dirname "$docker_path")"
+    export PATH="$(echo $PATH | sed -e "s%$DOCKER_DESKTOP_PATH/:%%g;s%$DOCKER_DESKTOP_PATH:%%g")"
+  fi
 fi
 
 if [ -f "$DOCKER_TOOLBOX_PATH"/docker-machine.exe ]; then
