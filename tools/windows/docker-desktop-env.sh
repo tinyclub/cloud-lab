fab1b2cda7cfe9a0e139393538449f7c5ba6c6e7
Wu Zhangjin <falcon@tinylab.org>
2021-12-18 05:26:52 +0800

    tools: clean up docker desktop env
    Signed-off-by: Wu Zhangjin <falcon@tinylab.org>

--- tools/windows/docker-desktop-env.sh
+++ tools/windows/docker-desktop-env.sh
@@ -36,15 +36,16 @@ if [ -d "$DOCKER_DESKTOP_PATH" ]; then
   DOCKER_DESKTOP="$DOCKER_DESKTOP_TOP_DIR/Docker Desktop.exe"
 
   if [ -f "$DOCKER_DESKTOP" ]; then
-    echo "Init Docker Desktop"
-    "$DOCKER_DESKTOP" &
+    echo "LOG: Start Docker Desktop"
+    "$DOCKER_DESKTOP"
+    echo "LOG: Done"
 
     echo $PATH | grep -q "$DOCKER_DESKTOP_PATH"
     if [ $? -ne 0 ]; then
-      echo "Add path of docker desktop to global PATH"
+      echo "LOG: Add path of docker desktop to global PATH"
       export PATH="$DOCKER_DESKTOP_PATH:$PATH"
     fi
   else
-    echo "No Docker Desktop found, you can try Docker Toolbox with tools/windows/docker-toolbox-env.sh"
+    echo "ERR: No Docker Desktop found, you can try Docker Toolbox with tools/windows/docker-toolbox-env.sh"
   fi
 fi
