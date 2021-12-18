8feaa5700cf6ee38113b9b280c5c83c495d5c99a
Wu Zhangjin <falcon@tinylab.org>
2021-12-18 19:59:40 +0800

    tools: not run docker desktop all the time
    Signed-off-by: Wu Zhangjin <falcon@tinylab.org>

--- tools/windows/docker-desktop-env.sh
+++ tools/windows/docker-desktop-env.sh
@@ -36,13 +36,16 @@ if [ -d "$DOCKER_DESKTOP_PATH" ]; then
   DOCKER_DESKTOP="$DOCKER_DESKTOP_TOP_DIR/Docker Desktop.exe"
 
   if [ -f "$DOCKER_DESKTOP" ]; then
-    echo "LOG: Start Docker Desktop"
-    "$DOCKER_DESKTOP"
-
     echo $PATH | grep -q "$DOCKER_DESKTOP_PATH"
     if [ $? -ne 0 ]; then
       echo "LOG: Add path of docker desktop to global PATH"
       export PATH="$DOCKER_DESKTOP_PATH:$PATH"
+    fi
+
+    docker ps >/dev/null 2>&1
+    if [ $? -ne 0 ] ; then
+      echo "LOG: Start Docker Desktop"
+      "$DOCKER_DESKTOP"
     fi
   else
     echo "ERR: No Docker Desktop found, you can try Docker Toolbox with tools/windows/docker-toolbox-env.sh"
